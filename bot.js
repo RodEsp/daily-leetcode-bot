1db5caf11072ab045a7636ddca9b077e67b4e109
rodesp <1084688+RodEsp@users.noreply.github.com>
2023-11-10 23:29:45 -0500

    fix: ensure the bot uses New York's timezone.

--- bot.js
+++ bot.js
@@ -5,8 +5,6 @@ import cron from 'node-cron';
 
 import { dailyCodingQuestions, problemListByCategory, questionOfTheDay } from './queries.js';
 
-const baseUrl = 'https://leetcode.com';
-
 // Use { zuliprc: 'zuliprc' } as a config object to use the zuliprc file instead of environment variables
 const zulipClient = await zulipInit({
 	username: process.env.ZULIP_USERNAME,
@@ -14,24 +12,35 @@ const zulipClient = await zulipInit({
 	realm: process.env.ZULIP_REALM,
 });
 
+const baseUrl = 'https://leetcode.com';
+const timezone = "America/New_York";
+
 class LeetCodeBot {
 	static async run () {
 		// Schedule the task to run every day at 10:00 AM
 		cron.schedule('0 10 * * *', async () => {
-			console.log('Getting leetcode questions for today');
+			const date = new Date().toLocaleDateString('en-US', { weekday: 'long', day: 'numeric', month: 'short', timeZone: timezone });
+			console.log(`Getting leetcode question for ${date}`);
 
 			try {
 				const data = (await request(`${baseUrl}/graphql`, questionOfTheDay)).activeDailyCodingChallengeQuestion;
 
-				const message = `${new Date().toLocaleDateString('en-US', { weekday: 'long', day: 'numeric', month: 'short' })}
+				const message = `${date}
 ### [${data.question.title}](${baseUrl}${data.link})
 > (${data.question.difficulty}) | ${data.question.topicTags.map((tag) => tag.name).join(', ')}`;
 
 				await this.postMessageToZulip(message);
+
+				console.log('Successfully posted message to Zulip.', message);
 			} catch (error) {
 				console.log('Error fetching the problem of the day:', error);
 			}
+		}, {
+			scheduled: true,
+			timezone
 		});
+
+		console.log(`Started successfully. Will post to Zulip everyday at 10 am, ${timezone} timezone`);
 	}
 
 	static async postMessageToZulip (message) {
